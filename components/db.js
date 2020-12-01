97dcee897d0923ddabae743feab89697933d3bb0
AngelGonzales <angelgonzales142@gmail.com>
2020-11-30 23:45:53 -0600

    creacion del componente de la base de datos

---
+++ components/db.js
@@ -0,0 +1,161 @@
+import React from "react";
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("basePokemons.db");
+
+// funcionalidades
+//selecciones de los datos de las  tablas
+const getPokemons = (setPokemonsFunc) =>{
+ db.transaction((tx)=>{
+     tx.executeSql("select * from Pokemons",
+     [],
+     (_, { rows: { _array } }) => {
+        setPokemonsFunc(_array);
+        },
+        (_t, error) => {
+            console.log("Error al momento de obtener los datos de los pokemons");
+            console.log(error);
+          },
+          (_t, _success) => {
+            console.log("Informacion de los pokemons obtenidas");
+          }
+     );
+ });
+};
+
+const getAtaques = (setPokemonsAtackFunc) =>{
+    db.transaction((tx)=>{
+        tx.executeSql("select * from Ataques",
+        [],
+        (_, { rows: { _array } }) => {
+           setPokemonsAtackFunc(_array);
+           },
+           (_t, error) => {
+               console.log("Error al momento de obtener los datos de los ataques de los pokemons");
+               console.log(error);
+             },
+             (_t, _success) => {
+               console.log("Informacion de los ataques pokemons obtenidas");
+             }
+        );
+    });
+   };
+   
+
+
+   
+   const getUsuarios = (setUserFunc) =>{
+    db.transaction((tx)=>{
+        tx.executeSql("select * from Usuarios",
+        [],
+        (_, { rows: { _array } }) => {
+           setUserFunc(_array);
+           },
+           (_t, error) => {
+               console.log("Error al momento de obtener los datos de usuarios");
+               console.log(error);
+             },
+             (_t, _success) => {
+               console.log("Informacion de los datos de usuarios obtenidos");
+             }
+        );
+    });
+   };
+//
+
+const updateUsuarios = (Value1, Value2) =>{
+    db.transaction((tx)=>{
+        tx.executeSql("update Usuarios set userName = ? where id= ? ",
+        [Value1,Value2],
+           (_t, error) => {
+               console.log("Error al momento de obtener los datos de usuarios");
+               console.log(error);
+             },
+             (_t, _success) => {
+               console.log("Informacion de los datos de usuarios obtenidos");
+             }
+        );
+    });
+   };
+
+   const updateUsuarios = (Value1) =>{
+    db.transaction((tx)=>{
+        tx.executeSql("delete from Usuarios where = ? ",
+        [Value1],
+           (_t, error) => {
+               console.log("Error al momento de obtener los datos de usuarios");
+               console.log(error);
+             },
+             (_t, _success) => {
+               console.log("Informacion de los datos de usuarios obtenidos");
+             }
+        );
+    });
+   };
+
+
+
+// creacion de la tabla de pokemons
+
+const setupPokemonsasync = async () => {
+    return new Promise((resolve, reject) => {
+      db.transaction(
+        (tx) => {
+          tx.executeSql(
+            "create table if not exists Pokemons (id integer primary key not null, nombre text,tipo text,descripcion text, ataque integer,defensa integer, velocidad integer, salud integer, habilidad integer, altura integer, peso integer)"
+          );
+        },
+        (_t, error) => {
+          console.log("Error al momento de crear la table");
+          console.log(error);
+          reject(error);
+        },
+        (_t, success) => {
+          resolve(success);
+        }
+      );
+    });
+  };
+   // funcion para crear tabla de ataques
+  const setupAtaquesAsync = async () => {
+    return new Promise((resolve, reject) => {
+      db.transaction(
+        (tx) => {
+          tx.executeSql(
+            "create table if not exists Ataques (id integer primary key not null, userName text, pokemonUsado text, pokemonContricante text, tiempo integer,saludJugador integer, saludComputador integer, puntos integer)"
+          );
+        },
+        (_t, error) => {
+          console.log("Error al momento de crear la table");
+          console.log(error);
+          reject(error);
+        },
+        (_t, success) => {
+          resolve(success);
+        }
+      );
+    });
+  };
+
+     // funcion para crear tabla de usuarios
+     const setupAtaquesAsync = async () => {
+        return new Promise((resolve, reject) => {
+          db.transaction(
+            (tx) => {
+              tx.executeSql(
+                "create table if not exists Usuarios (id integer primary key not null, userName text)"
+              );
+            },
+            (_t, error) => {
+              console.log("Error al momento de crear la table");
+              console.log(error);
+              reject(error);
+            },
+            (_t, success) => {
+              resolve(success);
+            }
+          );
+        });
+      };
+    
+      
